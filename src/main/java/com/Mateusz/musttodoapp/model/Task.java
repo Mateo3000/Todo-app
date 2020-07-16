cbae4ed8f1c34a2fcbe3e0dd04b9b0f25bd081cf
Programowanie <mateusz.gumieniak@wp.pl>
2020-07-16 18:51:13 +0200

    First commit

---
+++ src/main/java/com/Mateusz/musttodoapp/model/Task.java
@@ -0,0 +1,46 @@
+package com.Mateusz.musttodoapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @NotBlank(message = "Task's description must not be empty")
+    private String description;
+    private boolean done;
+
+    public Task() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    void setDone(final boolean done) {
+        this.done = done;
+    }
+}
---
+++ src/test/java/com/Mateusz/musttodoapp/MustTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mateusz.musttodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
