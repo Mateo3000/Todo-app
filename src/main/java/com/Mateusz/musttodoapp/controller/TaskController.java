7f447b8ff3a2de0bcb470ed7760c600b9881c537
Mateo3000 <mateusz.gumieniak@wp.pl>
2020-07-18 18:11:57 +0200

    "Create next migrations and create PrePersist and PreUpdated methods"

--- src/main/java/com/Mateusz/musttodoapp/controller/TaskController.java
+++ src/main/java/com/Mateusz/musttodoapp/controller/TaskController.java
@@ -7,12 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -57,8 +53,19 @@ class TaskController {
         if (!repository.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
-        toUpdate.setId(id);
-        repository.save(toUpdate);
+        repository.findById(id)
+                .ifPresent(task -> {
+                    task.updateFrom(toUpdate);
+                        });
+        return ResponseEntity.noContent().build();
+    }
+    @Transactional
+    @PatchMapping("/tasks/{id}")
+    public ResponseEntity<?> toggleTask(@PathVariable int id) {
+        if (!repository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        repository.findById(id).ifPresent(task -> task.setDone(!task.isDone()));
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/Mateusz/musttodoapp/model/Task.java
+++ src/main/java/com/Mateusz/musttodoapp/model/Task.java
@@ -1,21 +1,23 @@
 package com.Mateusz.musttodoapp.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     @NotBlank(message = "Task's description must not be empty")
     private String description;
     private boolean done;
+    @Column
+    private LocalDateTime deadline;
+    @Embedded
+    private Audit audit = new Audit();
 
     public Task() {
     }
@@ -24,7 +26,7 @@ public class Task {
         return id;
     }
 
-    public void setId(final int id) {
+    void setId(final int id) {
         this.id = id;
     }
 
@@ -40,7 +42,23 @@ public class Task {
         return done;
     }
 
-    void setDone(final boolean done) {
+    public void setDone(final boolean done) {
         this.done = done;
     }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public void updateFrom (final Task source) {
+        description = source.description;
+        done = source.done;
+        deadline = source.deadline;
+    }
+
+
 }
